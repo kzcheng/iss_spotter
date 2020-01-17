515ee2cfc470dd44fc07f1366ae9c9e789c349c6
kzcheng <kevinzifancheng@gmail.com>
2020-01-16 16:13:12 -0800

    created fetchCoordsByIP method

--- iss.js
+++ iss.js
@@ -10,10 +10,31 @@ const request = require(`request`);
  */
 const fetchMyIP = function(callback) {
   request(`https://api.ipify.org/?format=json`, (error, response, body) => {
+    // inside the request callback ...
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
     if (body) {
-      callback(error, JSON.parse(body).ip);
+      callback(null, JSON.parse(body).ip);
     }
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
