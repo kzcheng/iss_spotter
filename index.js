c8fe99925ac2b817b7afdf49138237f5f813eff3
kzcheng <kevinzifancheng@gmail.com>
2020-01-17 09:43:52 -0800

    progress

--- index.js
+++ index.js
@@ -35,8 +35,7 @@ const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+    console.log(`Next pass at ${datetime} for ${pass.duration} seconds!`);
   }
 };
 
